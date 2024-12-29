deea56423f1cb7948d9a98cd6f1d1f64914e8198
NickStepan <NickStepan>
2024-12-29 19:22:50 +0200

    New Files, Models and Views created.

---
+++ tasks_tracker/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Task(models.Model):
+    
+    STATUS_CHOICES = [
+    ("todo", "To Do"),
+    ("inprogress", "In progress"),
+    ("done", "Done"),
+    ]
+
+    PRIORITY_CHOICES = [
+        ("low", "Low"),
+        ("medium", "Medium"),
+        ("high", "High"),
+        ("critical", "Critical"),
+    ]
+
+    title = models.CharField(max_length=70)
+    description = models.TextField()
+    status = models.CharField(max_length=150, choices=STATUS_CHOICES, default="todo")
+    priority = models.CharField(max_length=150, choices=PRIORITY_CHOICES, default="medium")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    
